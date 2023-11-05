f72dbb7a720a8195abc64c51ddb81e8bdf9a7aca
Aaron Robinsom <airobinson0025@gmail.com>
2023-11-05 13:52:55 -0500

    feat: navbar change

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -7,8 +7,8 @@ import { IoSearchCircle } from 'react-icons/io5'
 
 const Navbar = () => {
   return (
-    <header className='font-nunito text-lg text-lime-600 flex justify-between items-center p-3 bg-neutral-900 shadow-sm shadow-neutral-100/40 z-1000 fixed w-full'>
-      <div className='flex space-x-5'>
+    <header className='font-nunito text-lg text-lime-600 tracking-wide flex justify-between items-center p-3 bg-neutral-900 shadow-sm shadow-neutral-100/40 z-1000 fixed w-full'>
+      <div className='flex space-x-8'>
         <Link href="/" className='text-[1.35rem]  font-semibold'>Munchie Map</Link>
         <div className='flex items-center space-x-1'>
           <ImLocation size={22} color='white'/>
@@ -16,14 +16,20 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className='flex items-center border-[1px] border-neutral-200 rounded-full pl-2'>
+      {/* <div className='flex items-center border-[1px] border-neutral-400 rounded-full pl-2'>
         <input 
         placeholder='Search Resturants'
         className='flex-grow bg-transparent outline-none placeholder-gray-400 rounded-md px-3'/>
         <IoSearchCircle size={40} color='#65a30d' className=' cursor-pointer'/>
-      </div>
+      </div> */}
 
-      <div className='flex items-center space-x-4'>
+      <div className='flex items-center space-x-6'>
+        <div className='flex items-center border-[1px] border-neutral-400 rounded-full pl-2'>
+            <input 
+            placeholder='Search Resturants'
+            className='flex-grow bg-transparent outline-none placeholder-gray-400 rounded-md px-3'/>
+            <IoSearchCircle size={40} color='#65a30d' className=' cursor-pointer'/>
+        </div>
         <Link href='/signup'>Sign Up</Link>
         <BiMenu size={35} color='white' className='cursor-pointer'/>
       </div>
